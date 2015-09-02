bfcefd984848dc942078eedd68564ad4d56bebc5
Sergey Maruda <maruda@ukr.net>
2015-09-02 10:53:04 +0300

    added initial version of duplicated files finder application

---
+++ DuplicatesFinder/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace DuplicatesFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.labelDirectory = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelDuplicatedSize = new System.Windows.Forms.Label();
+            this.treeViewDuplicatedFiles = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // labelDirectory
+            // 
+            this.labelDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelDirectory.AutoSize = true;
+            this.labelDirectory.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelDirectory.Location = new System.Drawing.Point(12, 454);
+            this.labelDirectory.Name = "labelDirectory";
+            this.labelDirectory.Size = new System.Drawing.Size(100, 15);
+            this.labelDirectory.TabIndex = 13;
+            this.labelDirectory.Text = "<Choose directory>";
+            this.labelDirectory.Click += new System.EventHandler(this.labelDirectory_Click_2);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(590, 448);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 454);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Duplicated files size:";
+            // 
+            // labelDuplicatedSize
+            // 
+            this.labelDuplicatedSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelDuplicatedSize.AutoSize = true;
+            this.labelDuplicatedSize.Location = new System.Drawing.Point(258, 454);
+            this.labelDuplicatedSize.Name = "labelDuplicatedSize";
+            this.labelDuplicatedSize.Size = new System.Drawing.Size(37, 13);
+            this.labelDuplicatedSize.TabIndex = 16;
+            this.labelDuplicatedSize.Text = "<size>";
+            // 
+            // treeViewDuplicatedFiles
+            // 
+            this.treeViewDuplicatedFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeViewDuplicatedFiles.Location = new System.Drawing.Point(12, 12);
+            this.treeViewDuplicatedFiles.Name = "treeViewDuplicatedFiles";
+            this.treeViewDuplicatedFiles.Size = new System.Drawing.Size(653, 430);
+            this.treeViewDuplicatedFiles.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(301, 454);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Mb";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(497, 448);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(87, 23);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar.TabIndex = 19;
+            this.progressBar.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 478);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.treeViewDuplicatedFiles);
+            this.Controls.Add(this.labelDuplicatedSize);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelDirectory);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "DuplicatesFinder";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label labelDirectory;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelDuplicatedSize;
+        private System.Windows.Forms.TreeView treeViewDuplicatedFiles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ DuplicatesFinder/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace DuplicatesFinder
+{
+    public partial class Form1 : Form
+    {
+        private Object sync = new Object();
+        private Thread m_thread = null;
+        private List<List<string>> duplicated_gorupds = new List<List<string>>();
+        private SortedDictionary<long, List<string>> files_by_sizes = new SortedDictionary<long, List<string>>();
+        private volatile bool thread_started = false;
+        private string m_dir_for_processing;
+        long totalLength = 0;
+
+        public delegate void OnDuplicatedGroupFound(List<string> group);
+
+        public OnDuplicatedGroupFound deleg = null;
+
+
+
+        //--------------------------------------------------------------------------------------------
+        Dictionary<string, List<string>> FindDuplicated(List<string> input_files)
+        {
+            var res = new HashSet<string>();
+            var id_path = new Dictionary<string, List<string>>();
+
+            var inp_files_set = new HashSet<string>(input_files);
+
+            foreach(var file_path in input_files)
+            {
+                if (thread_started == false)
+                    break;
+
+                byte[] hash;
+
+                using (var md5_calculator = SHA1.Create())
+                {
+                    const int buffer_size = 8 * 1024;
+                    using (var stream = new BufferedStream(File.OpenRead(file_path), buffer_size))
+                    {
+                        hash = md5_calculator.ComputeHash(stream);
+                    }
+
+                }
+
+                var sb = new StringBuilder();
+                for (int i = 0; i < hash.Length; ++i)
+                {
+                    sb.Append(hash[i].ToString("x2"));
+                }
+                var hash_str = sb.ToString();
+                List<string> str;
+                if(id_path.TryGetValue(hash_str, out str))
+                {
+                    str.Add(file_path);
+                }
+                else
+                {
+                    str = new List<string>();
+                    str.Add(file_path);
+                    id_path.Add(hash_str, str);
+                }
+            }
+            return id_path;
+        }
+
+        //--------------------------------------------------------------------------------------------
+        void ScanDirectory(string sDir)
+        {
+            try
+            {
+                var files = Directory.GetFiles(sDir, "*.*");
+                foreach (string f in files)
+                {
+                    if (thread_started == false)
+                        break;
+
+                    var finfo = new FileInfo(f);
+
+                    List<string> list = null;
+                    if (files_by_sizes.TryGetValue(finfo.Length, out list))
+                    {
+                        list.Add(f);
+                    }
+                    else
+                    {
+                        list = new List<string>();
+                        list.Add(f);
+                        files_by_sizes.Add(finfo.Length, list);
+                    }
+                }
+
+            }
+            catch (System.Exception)
+            {
+            }
+
+            try
+            {
+                var dirs = Directory.GetDirectories(sDir);
+                foreach (string d in dirs)
+                {
+                    ScanDirectory(d);
+                }
+            }
+            catch (System.Exception)
+            {
+            }
+
+
+
+        }
+
+        //--------------------------------------------------------------------------------------------
+        public Form1()
+        {
+            InitializeComponent();
+            deleg = addDuplicatedGroup;
+        }
+
+        //--------------------------------------------------------------------------------------------
+        private List<string> getFiles()
+        {
+            List<string> res = null;
+
+            lock (sync)
+            {
+                if(duplicated_gorupds.Count > 0)
+                {
+                    res = duplicated_gorupds[duplicated_gorupds.Count - 1];
+                    duplicated_gorupds.RemoveAt(duplicated_gorupds.Count - 1);
+                }
+            }
+            return res;
+        }
+
+        //--------------------------------------------------------------------------------------------
+        private void FindDuplicatedGroups()
+        {
+            thread_started = true;
+            files_by_sizes.Clear();
+            ScanDirectory(m_dir_for_processing);
+
+            foreach (var entry in files_by_sizes.Reverse())
+            {
+                if (entry.Value.Count > 1)
+                {
+                    var res = FindDuplicated(entry.Value);
+
+                    foreach (var entry2 in res)
+                    {
+                        if (entry2.Value.Count > 1)
+                        {
+                            lock (sync)
+                            {
+                                duplicated_gorupds.Add(entry2.Value);
+                            }
+                        }
+                    }
+                }
+            }
+            thread_started = false;
+        }
+
+        //--------------------------------------------------------------------------------------------
+        private void addDuplicatedGroup(List<string> group)
+        {
+            long сLength = -1;
+
+            var finfo = new FileInfo(group[0]);
+            var length_mb = ((double)finfo.Length / 1024 / 1024);
+            var size = string.Format("{0:0.00}", length_mb);
+            var root = treeViewDuplicatedFiles.Nodes;
+            var num_nodes = root.Count;
+            var node = root.Add("Group " + num_nodes + 1 + ". File size: " + size + " MB");
+
+            foreach (var a in group)
+            {
+                finfo = new FileInfo(a);
+                //node.;
+                node.Nodes.Add(a);
+                if (сLength != -1)
+                    сLength += finfo.Length;
+                else
+                    сLength = 0;
+            }
+            node.Expand();
+
+            totalLength += сLength;
+            labelDuplicatedSize.Text = string.Format("{0:0.00}", ((double)totalLength / 1024 / 1024));
+        }
+
+        //--------------------------------------------------------------------------------------------
+        private void labelDirectory_Click_2(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                this.labelDirectory.Text = fbd.SelectedPath;
+            }
+        }
+
+        //--------------------------------------------------------------------------------------------
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m_dir_for_processing = labelDirectory.Text;
+
+            DialogResult answer = DialogResult.None;
+
+            if (m_thread != null && m_thread.IsAlive)
+                answer = MessageBox.Show(this, "Terminate and start over?", "", MessageBoxButtons.YesNo);
+
+            if(answer == DialogResult.Yes)
+            {
+                TerminateExecution();
+            }
+
+            if (m_thread == null || !m_thread.IsAlive)
+            {
+                StartExecution();
+                InitUIUpdates();
+            }
+        }
+
+
+        //--------------------------------------------------------------------------------------------
+        private void StartExecution()
+        {
+            m_thread = new Thread(FindDuplicatedGroups);
+            m_thread.Start();
+        }
+
+        //--------------------------------------------------------------------------------------------
+        private void InitUIUpdates()
+        {
+            treeViewDuplicatedFiles.Nodes.Clear();
+            var timer = new System.Windows.Forms.Timer { Interval = 100 };
+            totalLength = 0;
+            timer.Tick += delegate
+            {
+                if (!progressBar.Visible)
+                    progressBar.Show();
+                List<string> res = null;
+                while ((res = getFiles()) != null)
+                {
+                    deleg(res);
+                    if (thread_started == true)
+                        break;
+                }
+
+                if (thread_started == false)
+                {
+                    progressBar.Hide();
+                    thread_started = true;
+                    timer.Stop();
+                }
+            };
+
+            timer.Start();
+        }
+
+
+        //--------------------------------------------------------------------------------------------
+        private void TerminateExecution()
+        {
+            if (m_thread != null && m_thread.IsAlive)
+            {
+                thread_started = false;
+                m_thread.Join();
+            }
+        }
+
+        //--------------------------------------------------------------------------------------------
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            TerminateExecution();
+        }
+
+    }
+}
